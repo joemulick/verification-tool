0c9e745c2bff028188ad0a04c56689157332c668
JoeMulick <joemulick@gmail.com>
2018-05-08 22:52:50 -0700

    Login component state set up on login

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -5,15 +5,51 @@ class Login extends Component {
         super();
         this.state = {
             email: '',
-            password: ''
+            password: '',
+            errors: {}
+        };
 
-        }
-
+        this.onChange = this.onChange.bind(this);
     }
+
+    onChange(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
     render(){
         return (
-            <div>
-                <h1>Login</h1>
+            <div className="login">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <h1 className="display-4 text-center">Log In</h1>
+                            <p className="lead text-center">Log in access for gatheroloie admin only</p>
+                                <form action="dashboard.html">
+                                    <div className="form-group">
+                                        <input 
+                                            type="email" 
+                                            className="form-control form-control-lg" 
+                                            placeholder="Email Address" 
+                                            name="email"
+                                            value={this.state.email}
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <input 
+                                            type="password" 
+                                            className="form-control form-control-lg" 
+                                            placeholder="Password" 
+                                            name="password"
+                                            value={this.state.password}
+                                            onChange={this.onChange} 
+                                        />
+                                    </div>
+                                    <input type="submit" className="btn btn-info btn-block mt-4" />
+                                </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
