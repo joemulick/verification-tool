a98962a52dfbf6ab932069894f153d9059e0c1c3
JoeMulick <joemulick@gmail.com>
2018-05-16 20:07:55 -0700

    fixed spelling

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,11 +5,11 @@ class Navbar extends Component {
         return (
             <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
               <div className="container">
-                <a className="navbar-brand" href="https://www.gatherologie.com/">Gatherology</a>
+                <a className="navbar-brand" href="https://www.gatherologie.com/">gatherologie</a>
                 
                   <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                      <a className="nav-link" href="https://www.gatherologie.com/">Back to Gatherology</a>
+                      <a className="nav-link" href="https://www.gatherologie.com/">Back to gatherologie</a>
                     </li>
                   </ul>
               </div>
