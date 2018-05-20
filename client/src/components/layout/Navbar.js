a3dd494d22b6874c237eed99cae2a25de4a9575d
JoeMulick <joemulick@gmail.com>
2018-05-20 16:34:47 -0700

    added cloudinary-react package

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import {Image} from 'cloudinary-react';
 
 class Navbar extends Component {
     render(){
         return (
             <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
               <div className="container">
-                <a className="navbar-brand" href="https://www.gatherologie.com/">gatherologie</a>
+                <a className="navbar-brand" href="https://www.gatherologie.com/">
+                  <Image publicId="gatherologie-main-logo_g8qtjl.png" >
+                  </Image>
+                </a>
                 
                   <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
