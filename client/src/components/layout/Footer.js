5675cf8f3cffa71da42034630080661fcaef961a
JoeMulick <joemulick@gmail.com>
2018-05-20 17:58:29 -0700

    brought in gatherologie logo and added some styling to the main landing page

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default () => {
     return (
-        <footer className="footer navbar-fixed-bottom bg-dark text-white mt-5 p-4 text-center">
+        <footer className="footer navbar-fixed-bottom mt-5 p-3 text-center nav-style">
             <div className="container">
-            Copyright &copy; {new Date().getFullYear()} Gatherology
+            <span className="nav-text">Copyright &copy; {new Date().getFullYear()} Gatherology</span>
             </div>
         </footer>
     )
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,16 +4,17 @@ import {Image} from 'cloudinary-react';
 class Navbar extends Component {
     render(){
         return (
-            <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+            <nav className="navbar navbar-expand-sm mb-4">
               <div className="container">
                 <a className="navbar-brand" href="https://www.gatherologie.com/">
-                  <Image publicId="gatherologie-main-logo_g8qtjl.png" >
-                  </Image>
+                <Image cloudName="ddsihrmda" publicId="gatherologie-main-logo_g8qtjl.png" width="180" crop="scale">
+
+                </Image>
                 </a>
                 
                   <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                      <a className="nav-link" href="https://www.gatherologie.com/">Back to gatherologie</a>
+                      <a className="nav-link" href="https://www.gatherologie.com/"><p className="nav-text">Back to gatherologie</p></a>
                     </li>
                   </ul>
               </div>
