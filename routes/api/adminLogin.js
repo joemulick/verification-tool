8df731136cde92bc9037c4bcb100b34cdd1040b8
JoeMulick <joemulick@gmail.com>
2018-05-20 21:13:51 -0700

    fixed error related to proptypes

--- routes/api/adminLogin.js
+++ routes/api/adminLogin.js
@@ -1,83 +1,83 @@
 const express = require('express');
 const router = express.Router();
-// Unsure If I need bcrypt (im thinking not)
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
 // Load Input Validation
-const ValidateLoginInput = require('../../validation/login');
+const validateLoginInput = require('../../validation/login');
 
-// Load Verify model
+// Load User model
 const Admin = require('../../models/Admin');
 
-
 // @route   GET api/users/test
-// @desc    Tests adminLogin route
+// @desc    Tests users route
 // @access  Public
-router.get('/test', (req, res) => res.json({ msg: 'adminLogin Works' }));
+router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 
 
-// @route GET api/admin/login
-// @desc Admin / Returning JWT Token
-// @access Public
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
 
-    const { errors, isValid } = ValidateCodeInput(req.body);
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
-    //Check Validation
-    if(!isValid) {
-        return res.status(400).json(errors);
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      errors.email = 'User not found';
+      return res.status(404).json(errors);
     }
 
-    const email = req.body.email;
-    const password = req.body.password;
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User Matched
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
 
-    // Find the user by email
-    Admin.findOne({email: email})
-    .then(user => {
-        //Check for user
-        if(!user) {
-            errors.email = 'User not found';
-            return res.status(404).json(errors)
-        }
-
-        //Check password
-        bcrypt.compare(password, user.password)
-            .then(isMatch => {
-                if(isMatch){
-                   // User Matched
-                    const payload = { id: user.id, name: user.name } // Create JWT Payload
-                   // Sign Token
-                   jwt.sign( payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
-                       res.json({
-                           sucess: true,
-                           token: 'Bearer ' + token
-                       })
-                   } )
-                } else {
-                    errors.password = 'Password incorrect';
-                    return res.status(400).json(errors);
-                }
-            })
+        // Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            });
+          }
+        );
+      } else {
+        errors.password = 'Password incorrect';
+        return res.status(400).json(errors);
+      }
     });
+  });
 });
 
-// @route GET api/users/current
-// @desc Return current user
-// @access Private
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
 router.get(
-    '/current',
-    passport.authenticate('jwt', {session: false}),
-    (req,res) => {
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
     res.json({
-        id: req.user.id,
-        name: req.user.name,
-        email: req.user.email
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
     });
-    }
+  }
 );
-
 
 module.exports = router;
